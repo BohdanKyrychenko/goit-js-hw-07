018f1a3bb4a4b5a69e9c40158c52dc9b2cb540c9
Bohdan <bogdan34110@gmail.com>
2024-08-29 18:40:33 +0300

    js-task

--- js/gallery.js
+++ js/gallery.js
@@ -1,48 +1,107 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
+const images = [
+	{
+		preview:
+			'https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820__480.jpg',
+		original:
+			'https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820_1280.jpg',
+		description: 'Hokkaido Flower',
+	},
+	{
+		preview:
+			'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg',
+		original:
+			'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg',
+		description: 'Container Haulage Freight',
+	},
+	{
+		preview:
+			'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785__340.jpg',
+		original:
+			'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785_1280.jpg',
+		description: 'Aerial Beach View',
+	},
+	{
+		preview:
+			'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619__340.jpg',
+		original:
+			'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619_1280.jpg',
+		description: 'Flower Blooms',
+	},
+	{
+		preview:
+			'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334__340.jpg',
+		original:
+			'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334_1280.jpg',
+		description: 'Alpine Mountains',
+	},
+	{
+		preview:
+			'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571__340.jpg',
+		original:
+			'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571_1280.jpg',
+		description: 'Mountain Lake Sailing',
+	},
+	{
+		preview:
+			'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272__340.jpg',
+		original:
+			'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg',
+		description: 'Alpine Spring Meadows',
+	},
+	{
+		preview:
+			'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255__340.jpg',
+		original:
+			'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg',
+		description: 'Nature Landscape',
+	},
+	{
+		preview:
+			'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843__340.jpg',
+		original:
+			'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg',
+		description: 'Lighthouse Coast Sea',
+	},
+];
+
+const galleryList = document.querySelector('.gallery');
+
+galleryList.insertAdjacentHTML('beforeend', generateGalleryItems(images));
+
+function generateGalleryItems(images) {
+	return images.map(image => {
+		return `
+			<li class="gallery-item">
+				<a class="gallery-link" href="${image.original}">
+					<img
+						class="gallery-image"
+						src="${image.preview}"
+						data-source="${image.original}"
+						alt="${image.description}"
+						width="360"
+						height="200"
+					/>
+				</a>
+			</li>
+		`;
+	}).join('');
 }
 
-const controls = document.querySelector('#controls');
-const input = controls.querySelector('input');
-const createButton = controls.querySelector('[data-create]');
-const destroyButton = controls.querySelector('[data-destroy]');
-const boxesContainer = document.querySelector('#boxes');
+// ==========================================================================================================================
 
-createButton.addEventListener('click', () => {
-  const amount = parseInt(input.value);
+document.addEventListener('click', documentActions);
+function documentActions(event) {
+	const targetElement = event.target;
 
-  if (amount >= 1 && amount <= 100) {
-    createBoxes(amount);
-    input.value = '';
-  } else {
-    alert('Please enter a number between 1 and 100');
-  }
-});
+	if (targetElement.closest('.gallery-link')) {
+		event.preventDefault();
+		const galleryImage = targetElement.closest('.gallery-image');
+		const galleryImageSource = galleryImage.getAttribute('data-source');
+		const galleryImageAlt = galleryImage.getAttribute('alt');
 
-destroyButton.addEventListener('click', destroyBoxes);
-
-function createBoxes(amount) {
-  destroyBoxes();
-
-  const elements = [];
-  let size = 30;
-
-  for (let i = 0; i < amount; i++) {
-    const div = document.createElement('div');
-    div.style.width = `${size}px`;
-    div.style.height = `${size}px`;
-    div.style.backgroundColor = getRandomHexColor();
-    div.style.marginTop = '10px';
-
-    elements.push(div);
-    size += 10;
-  }
-
-  boxesContainer.append(...elements);
-}
-
-function destroyBoxes() {
-  boxesContainer.innerHTML = '';
+		const instance = basicLightbox.create(`
+			<img class="enlarged-image" src="${galleryImageSource}" alt="${galleryImageAlt}" width="1112" height="640">
+		`);
+		instance.show();
+	}
 }
